01a55721dd469f8eb789ccc711518d276e3feda9
jmyu <dbwnsaud@gmail.com>
2024-06-25 17:40:17 +0900

    update movies

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link"
+import Movie from '../../components/movie'
+import styles from "../../styles/home.module.css"
 
 export const metadata = {
     title: "Home",
@@ -6,7 +7,7 @@ export const metadata = {
 
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
 
-async function getMovies(){
+async function getMovies() {
     const response = await fetch(API_URL)
     const json = await response.json()
     return json
@@ -15,14 +16,10 @@ async function getMovies(){
 export default async function HomePage() {
     const movies = await getMovies()
     return (
-        <div>
-            {movies.map((data)=>{
-                return(
-                    <li key={data.id}>
-                        <Link href={`/movies/${data.id}`}>{data.title}</Link>
-                    </li>
-                )
-            })}
+        <div className={styles.container}>
+            {movies.map((movie) => (
+                <Movie key={movie.id} title={movie.title} id={movie.id} poster_path={movie.poster_path} />
+            ))}
         </div>
     )
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,17 +1,27 @@
 import { Suspense } from "react"
 import MoviesInfo from "../../../../components/movie-info"
 import MovieVideos from "../../../../components/movie-videos"
+import { getMovie } from "../../../../components/movie-info"
 
+interface IParams {
+    params: { id: string }
+}
 
-export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
+export async function generateMetadata({ params: { id } }: IParams) {
+    const movie = await getMovie(id)
+    return {
+        title: movie.title
+    }
+}
+
+export default async function MovieDetailPage({ params: { id } }: IParams) {
     return (
         <div>
-            <h2>Movie Loading Page</h2>
             <Suspense fallback={<h1>Loading movie info</h1>}>
-                <MoviesInfo id={id}/>
+                <MoviesInfo id={id} />
             </Suspense>
             <Suspense fallback={<h1>Loading movie videos</h1>}>
-                <MovieVideos id={id}/>
+                <MovieVideos id={id} />
             </Suspense>
         </div>
     )
