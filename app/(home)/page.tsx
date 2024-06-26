84ec3644df7b894bec1035d4b9c03d782444c715
jmyu <dbwnsaud@gmail.com>
2024-06-26 17:07:12 +0900

    start build:)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import Movie from '../../components/movie'
 import styles from "../../styles/home.module.css"
-import { API_URL } from "../../components/common"
+import { API_URL } from "../constants"
 
 export const metadata = {
     title: "Home",
